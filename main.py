752ee3c2bad7873a045ab1a3c472949dd9de9ce7
Marcos Gomes <marcosgomescostavini@outlook.com>
2024-08-15 00:21:08 -0300

    I've added some functions to it like show taks and create a looping

--- main.py
+++ main.py
@@ -1,44 +1,77 @@
-import os,random
+import os
 
 tasks = []
 
 print(tasks)
+
+def show_tasks():
+     for i,value in enumerate(tasks, start=1):
+          print(f"Task number {i}: {value}")
+
 def clean_screen():
     if os.name == 'nt':
         os.system('cls')
     else:
         os.system('clear')
 
+def show_options():
+    print("""
+           1 - Create Tasks
+           2 - Remove Tasks
+           3 - Show All Tasks
+           4 - Change a Task
+           5 - Exit App
+           """)
+    print("What option do you want to do?")
+
 def create_tasks():
     print("What do you want to do?")
     activity = input('Write here?\n').lower()
     tasks.append(activity)
 
-def remove_tasks(task):
-    print("May be you don't know what task you wanna remove, I'll show you all the options")
-    for i in tasks:
-        position = tasks.index(i)
-        print(f"{position + 1} is {i}")
+def remove_tasks():
+    print("Maybe you don't know what task you wanna remove, I'll show you all the options")
+    for i,value in enumerate(tasks,start =1):
+        print(f"{i} is {value}")
         print("Type the number of the task you want to remove")
+
     try:
-        number = int(input())
-        numberTaskToBeRemoved = number - 1
-        taskToBeRemovedPosition = tasks[numberTaskToBeRemoved]
-        print(f'Are you right you want to remove {tasks[numberTaskToBeRemoved]} Y/N')
-        choice = input('Y/N\n').lower()
-        if choice == "y":
-            tasks.remove(taskToBeRemovedPosition)
-        else:
-            print("Not action take, be easy")
-        print(tasks)
-
+        number = int(input()) - 1
     except ValueError:
-        print("Type a correct value please")
+            print("Type a correct value please")
 
-create_tasks()
-result = create_tasks
-remove_tasks(result)
-# def show_tasks():
+    if 0 <= number < len(tasks):
+            taskToBeRemovedPosition = tasks[number]
+            print(f'Are you right you want to remove {tasks[number]} Y/N')
+            choice = input('Y/N\n').lower()
+            if choice == "y":
+                tasks.remove(taskToBeRemovedPosition)
+            else:
+                print("Not action take, be easy")
+            print(tasks)
+    else:
+        print("Out of range")
+
+while True:
+    show_options()
+    try:
+        choice = int(input())
+    except ValueError:
+        print("Type a valid value please")
+    match choice:
+        case 1:
+            create_tasks()
+            clean_screen()
+        case 2:
+              remove_tasks()
+              clean_screen
+        case 3:
+              show_tasks()
+        case 4:
+              print("In procss")
+        case 5:
+              print("Thank You, break")
+              break
 
 # def change_task():
 
